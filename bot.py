895be85414177536ab3a4327c25880a97f2faee7
QwanXiong <danichist@yandex.ru>
2024-09-25 13:12:16 +0300

    Added webhooks
    -Added webhooks based on the example from https://docs.aiogram.dev/en/latest/dispatcher/webhook.html
    -Turned main_webhook() into synchronous function

--- bot.py
+++ bot.py
@@ -2,7 +2,8 @@ import logging
 import os
 import sys
 from typing import Optional, Union, List, Dict
-
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 import asyncio
 from aiogram import Bot, Dispatcher, Router
 from aiogram import F, types
@@ -14,6 +15,7 @@ import aiogram.utils.markdown as fmt
 from aiogram.fsm.state import State, StatesGroup
 
 from aiohttp import web
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 import argparse
 
 def build_arg_parser() -> argparse.ArgumentParser:
@@ -24,9 +26,16 @@ def build_arg_parser() -> argparse.ArgumentParser:
                         choices=['polling','webhook'])
     return parser
 
+WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_PORT = 8080
+WEBHOOK_PATH = os.environ.get('WEBHOOK_PATH', '')
+
+WEBHOOK_SECRET = os.environ.get('WEBHOOK_SECRET', '')
+#BASE_WEBHOOK_URL = 
+BASE_WEBHOOK_URL = os.environ.get('BASE_WEBHOOK_URL', '')
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN', '')
 
-bot = Bot(token=TELEGRAM_TOKEN)
+bot = Bot(token=TELEGRAM_TOKEN,default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 router = Router()
 
 class Context(StatesGroup):
@@ -222,12 +231,24 @@ async def finalize_keywords_selection(callback: CallbackQuery, state: FSMContext
     await bot.send_message(callback.message.chat.id, LEXICON[FINALIZE_KEYWORDS_CBK])
     await state.clear()
 
-
-async def set_main_menu(bot: Bot):
+async def set_main_menu_polling(bot: Bot):
     main_menu = [
         BotCommand(command='/help', description='The description of how to use the bot'),
     ]
 
+    #elif api_type == 'polling':
+    #    asyncio.run(main_polling())
+    await bot.set_my_commands(main_menu)
+
+async def set_main_menu_webhook(bot: Bot):
+    main_menu = [
+        BotCommand(command='/help', description='The description of how to use the bot'),
+    ]
+
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
+    logging.debug("Webhook was set")
+    #elif api_type == 'polling':
+    #    asyncio.run(main_polling())
     await bot.set_my_commands(main_menu)
 
 
@@ -235,12 +256,61 @@ async def main_polling():
     logging.debug("Entered main_polling()")
     dp = Dispatcher()
     dp.include_router(router)
-    dp.startup.register(set_main_menu)
+    dp.startup.register(set_main_menu_polling)
     await dp.start_polling(bot)
 
-async def main_webhook():
+###async def on_startup(bot: Bot) -> None:
+#    # If you have a self-signed SSL certificate, then you will need to send a public
+#    # certificate to Telegram
+#    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
+#
+async def on_startup():
+    await bot.set_webhook(f"{BASE_WEBHOOK_URL}{WEBHOOK_PATH}", secret_token=WEBHOOK_SECRET)
+
+
+def main_webhook():
     logging.debug("Entered main_webhook()")
-    pass
+
+    dp = Dispatcher()
+    dp.include_router(router)
+    dp.startup.register(set_main_menu_webhook)
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    app = web.Application()
+    webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    setup_application(app, dp, bot=bot)
+    web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    #asyncio.run(web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT))
+
+
+#TODO: Does the main function have to be asynchronous? 
+async def depr_main_webhook():
+    logging.debug("Entered main_webhook()")
+
+    dp = Dispatcher()
+    dp.include_router(router)
+    dp.startup.register(set_main_menu_webhook)
+    webhook_requests_handler = SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot,
+        secret_token=WEBHOOK_SECRET,
+    )
+    #NOTE: https://stackoverflow.com/questions/53465862/python-aiohttp-into-existing-event-loop
+    app = web.Application()
+    #  webhook_requests_handler.register(app, path=WEBHOOK_PATH)
+    #setup_application(app, dp, bot=bot)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    site = web.TCPSite(runner,host=WEB_SERVER_HOST,port=WEB_SERVER_PORT)
+    await site.start()
+    print("===== Running ======")
+    await asyncio.sleep(3600)
+        #await asyncio.Event().wait()
+    #await web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+    #asyncio.run(web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT))
 
 if __name__ == "__main__":
     parser = build_arg_parser()
@@ -257,11 +327,16 @@ if __name__ == "__main__":
     api_type = args.api_type
     try:
         if api_type == 'webhook':
-            asyncio.run(main_webhook())
+            main_webhook()
+            #asyncio.run(main_webhook())
+        #main_webhook()
+        #web.run_app(app, host=WEB_SERVER_HOST, port=WEB_SERVER_PORT)
+        #print(type(app))
+        #print(type())
         elif api_type == 'polling':
             asyncio.run(main_polling())
-    except:
-        print('Some exception occured!')
+    except Exception as e:
+        print('Some exception occured!',e)
 #    dp.include_router(router)
 #    dp.startup.register(set_main_menu)
 #    dp.run_polling(bot)
